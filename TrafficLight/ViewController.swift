b46333aeac5091eb767638ca881f156220565abd
Elena Sharipova <elena.sh.r@yandex.ru>
2023-03-17 16:53:59 +0300

    feat: traffic light app implemented

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lightViews: [UIView]!
+    @IBOutlet var toggleLightButton: UIButton!
+    
+    var activeColorIndex = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        toggleLightButton.layer.cornerRadius = 10
+        toggleLightButton.setTitle("Start".uppercased(), for: .normal)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        lightViews.forEach {
+            $0.layer.cornerRadius = $0.layer.bounds.width / 2
+            $0.clipsToBounds = true
+        }
+    }
+    
+    
+    @IBAction func toggleLightButtonTapped() {
+        toggleLightButton.setTitle("Next".uppercased(), for: .normal)
+        
+        if activeColorIndex >= 0 {
+            lightViews[activeColorIndex].alpha = 0.3
+        }
+        
+        activeColorIndex = (activeColorIndex == lightViews.count - 1)
+            ? 0
+            : activeColorIndex + 1
+        
+        lightViews[activeColorIndex].alpha = 1
+    }
 }
 
